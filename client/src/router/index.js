0baa8d38197cf44ef56cc1b4baffcf519424e5a9
Eduardo San Martin Morote <posva13@gmail.com>
2017-10-24 15:59:34 +0200

    Add some comments

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -19,6 +19,9 @@ const router = new Router({
       path: '/login',
       component: Login,
       meta: {
+        // the meta object can contain any information
+        // about the route that you may want to use
+        // elsewhere, like in beforeEach
         requiresNonAuth: true,
       },
     },
@@ -26,6 +29,7 @@ const router = new Router({
       path: '/signup',
       component: Signup,
       meta: {
+        // It's also reusable!
         requiresNonAuth: true,
       },
     },
@@ -39,14 +43,28 @@ const router = new Router({
   ],
 });
 
+// The callback passed to beforeEach will
+// get executed every time we try to navigate
+// There we can check the meta attribute
 router.beforeEach((to, from, next) => {
+  // Warning: This doesn't work for nested routes!
+  // If you have nested routes use the version at
+  // https://router.vuejs.org/en/advanced/meta.html
   if (to.meta.requiresAuth) {
+    // The navigation may happen before the root (new Vue in main.js)
+    // is created (hook where we call checkUser), to prevent that,
+    // we call it here
     checkUser(router.app.$root);
     if (router.app.$root.user) next();
     else
       next({
         path: '/login',
         query: {
+          // URL queries cannot contain specific characters
+          // like / or ?, so we use encodeURIComponent
+          // to transform it into a valide query value
+          // We will have to use the complementary function
+          // decodeURIComponent to get the orginial value
           redirect: encodeURIComponent(to.fullPath),
         },
       });
@@ -55,6 +73,12 @@ router.beforeEach((to, from, next) => {
   }
 });
 
+// This one is pretty much the opposite
+// We only want guests to access login and
+// signup pages. It's probably an error on
+// their end if they try to go to login or
+// signup while logged in, so we redirect
+// them to a more welcoming place :)
 router.beforeEach((to, from, next) => {
   if (to.meta.requiresNonAuth) {
     checkUser(router.app.$root);
