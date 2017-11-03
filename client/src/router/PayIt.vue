8ce2b79aa8740850187914c2c4d3d304f10de633
Eduardo San Martin Morote <posva13@gmail.com>
2017-11-03 16:16:02 +0100

    Add missing view component

---
+++ client/src/router/PayIt.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <button @click="checkout">Checkout</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  methods: {
+    checkout() {
+      // this.$checkout.close()
+      // is also available.
+      this.$checkout.open({
+        name: 'Shut up and take my money!',
+        currency: 'EUR',
+        amount: 100, // 10€
+        token: (token) => {
+          axios.post('http://localhost:3000/api/pay', { token })
+          .then(r => r.data)
+          .then(console.log)
+          .catch(console.error)
+        }
+      });
+    }
+  }
+}
+</script>
